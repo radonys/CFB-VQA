252cb7a184156ff1683a61a9cb536d3d31d38ba3
fortis <ravi.p15@iiits.in>
2018-01-20 16:45:06 +0530

    changes

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -4,7 +4,7 @@ from flask import render_template
 
 @app.route("/")
 def home():
-    return render_template('/app/src/index.html')
+    return render_template('index.html')
 
 # Uncomment to add a new URL at /new
 
