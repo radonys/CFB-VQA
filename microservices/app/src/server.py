8f24d92f802264e29937e58edddbe1a0232e915c
Vaishnav Murali <vaishnavmurali@gmail.com>
2018-01-20 18:53:42 +0530

    modified template
    Signed-off-by: Vaishnav Murali <vaishnavmurali@gmail.com>

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -2,15 +2,18 @@ from src import app
 # from flask import jsonify
 from flask import render_template,request
 from VQA import predict
+UPLOAD_FOLDER = '/path/to/the/uploads'
 @app.route("/", methods = ['GET', 'POST'])
 def home():
     if request.method == 'GET':
         return render_template('index.html')
     if request.method == 'POST':
         file1 = request.files['file']
-        
+        temp = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file1.save(temp)
         print file1,type(file1)
-        return render_template('index.html')
+        result = predict(temp)
+        return render_template('index.html',result=result)
 
 
 # Uncomment to add a new URL at /new
