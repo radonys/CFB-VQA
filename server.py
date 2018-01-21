74249e78eed347d0d573a4fa282da9afd4d75558
Vaishnav Murali <vaishnavmurali@gmail.com>
2018-01-22 01:38:29 +0530

    Added weights

--- server.py
+++ server.py
@@ -13,40 +13,19 @@ def home():
     print "Hello"
     print request.args,request.form
     if request.method == 'POST':
-        print request.args,request.form
+        # print request.args,request.form
         if "question" in request.form:
             print "POST FORM"
             file1 = request.files['file']
             ques = request.form["question"]
             temp = os.path.join(UPLOAD_FOLDER, ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])+'.jpg')
-            print temp
             file1.save(temp)
-            print file1,type(file1)
             result = predict(temp,ques)
             os.remove(temp)
-            print result
             return jsonify(result=result,success=True)
         else:
             print "NO question came"
             return "oops"
     else:
         return render_template('index.html')
-
-    # if request.method == 'POST':
-        # file1 = request.files['file']
-        # ques = request.post["question"]
-        # temp = os.path.join(app.config['UPLOAD_FOLDER'], 'image.jpg')
-        # print temp
-        # file1.save(temp)
-        # print file1,type(file1)
-        # result = predict(temp,ques)
-        # print result
-        # sys.stdout.flush()
-        # return render_template('index.html',result=result)
-
 sys.stdout.flush()
-# Uncomment to add a new URL at /new
-
-# @app.route("/json")
-# def json_message():
-#     return jsonify(message="Hello World")
